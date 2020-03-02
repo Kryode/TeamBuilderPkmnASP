05af2680d2b470acd5196cd21015dc232eb166bf
Anthony <anthony.guiock@gmail.com>
2020-03-02 20:56:21 +0100

    Cleanup front page + add sprites

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -7,29 +7,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
+using System.Text;
+using KryodeHelpers;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TeamBuilderPkmnASP.Models
 {
     public class Pokemon
     {
+        private string lowerName;
         [Name("id")]
         public int Id { get; set; }
         [Name("identifier")]
-        public string Name { get; set; }
+        public string Name { 
+            get 
+            {
+                return StringHelper.FirstCharToUpper(lowerName);
+            } 
+            set 
+            {
+                lowerName = value.ToLower();
+            }
+        }
+
+        public string Image { get; set; }
         
         //Csv construct
-        public Pokemon(int id, string identifier, int species_id, int height, int weight, int base_experience, int order, int is_default)
+        public Pokemon(int id, string identifier)
         {
             this.Id = id;
-            this.Name = identifier;
+            this.lowerName = identifier;
+            this.Image = DataPaths.Sprite + Id.ToString() + ".png";
         }
 
         public static Pokemon[] GetPokemons()
         {
             Pokemon[] pokemons;
-            using (var reader = new StreamReader("wwwroot/Data/pokemon.csv"))
+            using (var reader = new StreamReader(DataPaths.PokemonCsv))
             {
                 CsvConfiguration configuration = new CsvConfiguration(CultureInfo.InvariantCulture);
+                configuration.HeaderValidated = null;
                 using (var csv = new CsvReader(reader, configuration))
                 {
                     pokemons = csv.GetRecords<Pokemon>().ToArray();
